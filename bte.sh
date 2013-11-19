ea8b7b36ce7584becde9be9b49c835ad74f0abc4
bennet0496 <stuff.bbennet96@gmail.com>
2013-11-19 16:04:31 +0100

    forst commit

---
+++ bte.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+placeholder_replace_multiline () {
+	sed "s#$1#$( eval $2 | sed ':a;N;$!ba;s/\n/\\n/g')#" $3
+}
+placeholder_replace (){
+	sed "s/$1/$2/" $3
+}
+
+
+placeholder_replace PLATZ 'ps aux | grep term' file
+placeholder_replace PLATZ 'ps aux | grep firef' file
